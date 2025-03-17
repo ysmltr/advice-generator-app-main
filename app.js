27a3a0f99a32f95693c38322e2e115c82ed7b065
ysmltr <yesimltr1@gmail.com>
2025-03-17 23:21:12 +0300

    Add advice generator functionality and update HTML structure

---
+++ app.js
@@ -0,0 +1,20 @@
+const adviceResult = document.getElementById("advice-text");
+const resultBtn = document.getElementById("result-btn");
+const adviceNum = document.getElementById("advice-num");
+
+resultBtn.addEventListener("click", () => {
+    getAdvice();
+});
+
+function getAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+        .then(response => response.json())
+        .then(adviceData => {
+            const adviceObj = adviceData.slip;
+            adviceResult.innerHTML = `<p>${adviceObj.advice}</p>`;
+            adviceNum.innerHTML = `${adviceObj.id}`;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
